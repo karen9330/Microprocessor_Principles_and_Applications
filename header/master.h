645f1d175f3c775acd85e171dbf1c001442cf5ad
Wun Ci Huang <c24116095@gs.ncku.edu.tw>
2025-11-25 16:15:49 +0800

    Delete not used #define

--- header/master.h
+++ header/master.h
@@ -1,12 +1,6 @@
 #ifndef MASTER_H
 #define	MASTER_H
 
-#define SPI_DIR_SDO TRISCbits.RC5   // output to slave (data)
-#define SPI_DIR_CLK TRISCbits.RC3   // clock
-#define SPI_DIR_SDI TRISCbits.RC4   // input form slave (not used)
-#define SPI_DIR_CS  TRISEbits.RE2   // Master mode do not need to use Slave Select pin
-#define SPI_CS LATEbits.LE2
-
 #include <xc.h> 
 #include <stdint.h>
 #include <stdbool.h>
